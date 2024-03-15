2f0193b709fbb4374cf3ceb23eac187a28721101
kghstgrl <160274071+kghstgrl@users.noreply.github.com>
2024-03-15 21:09:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,11 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
     int c = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 c++;
+                break;
             }
         }
     }
@@ -14,7 +15,7 @@ int countPairs2(int* arr, int len, int value) {
     int c = 0;
     int left = 0;
     int right = len - 1;
-    while (left < right) {
+    while (left < right && left < len && right >= 0) {
         if (arr[left] + arr[right] == value) {
             c++;
             left++;
@@ -42,10 +43,13 @@ int binarySearch(int* arr, int l, int r, int x) {
 }
 int countPairs3(int* arr, int len, int value) {
     int c = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len - 1; i++) {
         int index = binarySearch(arr, i + 1, len - 1, value - arr[i]);
         if (index != -1 && arr[index] != arr[i]) {
             c++;
+            while (i < len – 1 && arr[i] == arr[i + 1]) {
+                i++;
+            }
         }
     }
     return c;
